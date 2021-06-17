8570399ab7a0d2b3d5c4115d984b5337340eb2e3
Patrik Olin <enmejl@gmail.com>
2021-06-17 15:18:27 +0200

    save old node id to gob-file when opening scratch pad so we can refocus the previously focused node when hiding scratch again

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"encoding/gob"
 	"flag"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
 )
 
 var hide bool
 var fetch bool
+var path = os.ExpandEnv("$HOME") + "/.bsp"
 
 func main() {
 	flag.BoolVar(&hide, "hide", false, "hide node")
@@ -32,16 +35,62 @@ func main() {
 }
 
 func hideNode() {
+	nodeID := readFromFile()
 	cmd := exec.Command("bspc", "node", "-g", "hidden")
 	cmd.Run()
+
+	focusCmd := exec.Command("bspc", "node", "-f", nodeID)
+	focusCmd.Run()
 }
 
 func fetchNode() {
+	saveCurrentNodeID()
+
 	out, err := exec.Command("bspc", "query", "-N", "-n", ".hidden").Output()
 	if err != nil {
 		log.Fatalln(err)
 	}
-	nid := strings.TrimSpace(string(out))
-	cmd := exec.Command("bspc", "node", nid, "-g", "hidden=off", "-f")
+	nodeID := strings.TrimSpace(string(out))
+
+	cmd := exec.Command("bspc", "node", nodeID, "-g", "hidden=off", "-f")
 	cmd.Run()
+}
+
+func saveCurrentNodeID() {
+	cmd, err := exec.Command("bspc", "query", "-N", "-n", ".focused").Output()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	writeToFile(strings.TrimSpace(string(cmd)))
+}
+
+func writeToFile(nodeID string) {
+	f, err := os.Create(path)
+	if err != nil {
+		log.Fatalln("Error opening/creating file: ", err)
+	}
+	encoder := gob.NewEncoder(f)
+
+	err = encoder.Encode(nodeID)
+	if err != nil {
+		log.Fatalln("Error encoding map: ", err)
+	}
+
+	f.Close()
+}
+
+func readFromFile() string {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalln(err)
+		return ""
+
+	}
+	defer file.Close()
+
+	var nodeID string
+	decoder := gob.NewDecoder(file)
+	decoder.Decode(&nodeID)
+
+	return nodeID
 }
